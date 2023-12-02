5f444d7b98bc1004f1fea6d6ce63634cbdc2a4f6
BennyBlanco88 <ben.langs@yahoo.com>
2023-12-02 11:40:31 -0800

    sync files to make application work

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -4,31 +4,31 @@ const fs = require ("fs");
 
 //get request to '/api/notes
 router.get('/api/notes', async (req, res) => {
-    const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
+    const dbJson = await JSON.parse(fs.readFileSync("Develop/db/db.json", "utf8"));
     res.json(dbJson);
 });
 
 // post request to '/api/notes
 router.post('/api/notes', (req, res) => {
-    const dbJson = JSON.parse(fs.readFileSync("db/db.json","utf8"));
+    const dbJson = JSON.parse(fs.readFileSync("Develop/db/db.json","utf8"));
     const newFeedback = {
         title: req.body.title,
-        text:req.body.text,
+        text: req.body.text,
         id: uuidv4(),
     };
     dbJson.push(newFeedback);
-    fs.writeFileSync("db/db.json",JSON.stringify(dbJson));
+    fs.writeFileSync("Develop/db/db.json",JSON.stringify(dbJson));
     res.json(dbJson);
 });
 
 router.delete('/api/notes/:id', (req, res) => {
-    let data = fs.readFileSync("db/db.json", "utf8");
+    let data = fs.readFileSync("Develop/db/db.json", "utf8");
     const dataJSON = JSON.parse(data);
     const newNotes = dataJSON.filter((note) => {
         return note.id !== req.params.id;
     });
-    fs.writeFileSync("db/db.json",JSON.stringify(newNotes));
-    req.json("Note deleted.");
+    fs.writeFileSync("Develop/db/db.json",JSON.stringify(newNotes));
+    res.json("Note deleted.");
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -4,9 +4,11 @@ const api_routes = require('./Develop/routes/api-routes')
 const PORT = process.env.PORT || 3001;
 const app = express ();
 
+
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 app.use(express.static("public"));
+app.use(express.static("Develop/public"));
 app.use(html_routes)
 app.use(api_routes)
 
