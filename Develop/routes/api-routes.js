4063ad5987d086f4f8e6e2daa3dd68d4c553e486
BennyBlanco88 <ben.langs@yahoo.com>
2023-12-02 09:13:36 -0800

    made chages to fix bugs

--- Develop/routes/api-routes.js
+++ Develop/routes/api-routes.js
@@ -3,7 +3,7 @@ const { v4: uuidv4 } = require('uuid');
 const fs = require ("fs");
 
 //get request to '/api/notes
-router.get('/api/notes', async (res, res) => {
+router.get('/api/notes', async (req, res) => {
     const dbJson = await JSON.parse(fs.readFileSync("db/db.json", "utf8"));
     res.json(dbJson);
 });
